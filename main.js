279fdbdc63baa2fb75fb7f6bed5edafcf08e4a9f
Kevin Mok <kevin.mok@live.ca>
2019-02-21 05:29:38 -0500

    Add food div's from json

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const log = console.log
 
-const itemDivs = document.querySelectorAll('.item')
-log(itemDivs)
+/* const itemDivs = document.querySelectorAll('.item')
 
 for (const itemDiv of itemDivs) {
 	itemDiv.className = 'item col-lg-4'
+} */
+function createNewRow() {
+	const curRow = document.createElement('div')
+	curRow.className = 'row'
+	return curRow
 }
+
+function createNewFoodDiv() {
+	const foodDiv = document.createElement('div')
+	foodDiv.className =   'item col-lg-4'
+	return foodDiv
+}
+
+const foodGridDiv = document.querySelector('#food-grid')
+
+const fruits = {"1":{"name":"Red Apple","store_name":"storeA","img":"imgs/apples.jpg","price":189},"2":{"name":"Yellow Lemon","store_name":"storeC","img":"imgs/lemon.jpg","price":192},"3":{"name":"Green Lemon","store_name":"storeC","img":"imgs/green-lemon.jpg","price":68},"4":{"name":"Green Apple","store_name":"storeC","img":"imgs/green-apple.jpg","price":159},"5":{"name":"Waterlemon","store_name":"storeA","img":"","price":196}}
+let curRow = createNewRow()
+foodGridDiv.appendChild(curRow)
+Object.keys(fruits).forEach(function(key, index) {
+	const foodDiv = createNewFoodDiv()
+	const foodImg = document.createElement('img')
+	foodImg.src = fruits[key]["img"]
+	log(foodImg)
+	foodDiv.appendChild(foodImg)
+	curRow.appendChild(foodDiv)
+})
 
 // The following code implements the feature where if you hover a dropdown
 // menu link, the dropdown options appear
