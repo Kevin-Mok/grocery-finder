3bac153d8f65ba3ed5f0d8f215470414068ac485
Calvin Lu <calvin.lu@mail.utoronto.ca>
2019-03-03 20:59:13 -0500

    Resolved an issue where password changes would not persist when switching between users. Admins will now be able to cancel password edits, and will not be able to see the passwords of other administrators. Updated administrator confirmation message to better highlight promotion consequences.

--- js/adminPage.js
+++ js/adminPage.js
@@ -19,13 +19,13 @@ class User {
 
 // Dummy user data for phase 1
 
-users.push(new User("Obama", "password", "Narnia", new Date("September 21, 2017 05:25:11"), new Date("February 20, 2018 06:21:33"), 'imgs/profile-pictures/obama.jpg', null, false));
-users.push(new User("ProGamer", "password", "Westeros", new Date("May 11, 2016 09:29:29"), new Date("May 11, 2016 09:29:29"),'imgs/profile-pictures/progamer.jpeg', null));
-users.push(new User("TimeTraveller", "password", "Essos",new Date("December 19, 2019 18:59:59"), new Date("December 20, 2019 17:10:02"), 'imgs/profile-pictures/timetraveller.jpeg', null, false));
+users.push(new User("Obama", "america", "Narnia", new Date("September 21, 2017 05:25:11"), new Date("February 20, 2018 06:21:33"), 'imgs/profile-pictures/obama.jpg', null, false));
+users.push(new User("ProGamer", "1336", "Westeros", new Date("May 11, 2016 09:29:29"), new Date("May 11, 2016 09:29:29"),'imgs/profile-pictures/progamer.jpeg', null));
+users.push(new User("TimeTraveller", "timeisaconstruct", "Essos",new Date("December 19, 2019 18:59:59"), new Date("December 20, 2019 17:10:02"), 'imgs/profile-pictures/timetraveller.jpeg', null, false));
 users.push(new User("Ronald", "password", "Smallville", new Date("August 5, 2017 17:21:22"), new Date("August 5, 2017 17:21:22"), 'imgs/profile-pictures/default.jpg', null, false));
-users.push(new User("Bucky", "password", "New Boston", new Date("January 6, 2016 01:45:00"), new Date("January 6, 2016 07:12:32"), 'imgs/profile-pictures/default.jpg', null, false));
+users.push(new User("Bucky", "chickensoup", "New Boston", new Date("January 6, 2016 01:45:00"), new Date("January 6, 2016 07:12:32"), 'imgs/profile-pictures/default.jpg', null, false));
 users.push(new User("Tony", "password", "Ba Sing Se", new Date("February 28, 2019 05:27:19"), new Date("January 19, 2019 05:27:19"), 'imgs/profile-pictures/default.jpg', null, false));
-users.push(new User("Barnes", "password", "Pluto", new Date("July 1, 2017 23:21:19"), new Date("July 4, 2017 23:59:59"), 'imgs/profile-pictures/barnes.jpeg', null, false));
+users.push(new User("Barnes", "pizza", "Pluto", new Date("July 1, 2017 23:21:19"), new Date("July 4, 2017 23:59:59"), 'imgs/profile-pictures/barnes.jpeg', null, false));
 
 
 // loadUsers fills the user listing box
@@ -197,13 +197,22 @@ function createAdminFields(isAdmin, parentDiv) {
 
 function promoteToAdmin(e) {
 	if (confirm("Are you sure you want to promote this user to administrator status?" +
-		"\nThis user will be able to access and modify user profiles. \n\n" +
-		"                                      (Press 'OK' to confirm)")) {
+		"\nThis user will be able to access and modify user profiles." +
+		"\nThis action cannot be undone, and you will not be able to revoke administrator privilege." + 
+		"\n\n                                      (Press 'OK' to confirm)")) {
 		const userToChange = e.target.parentElement.parentElement.user;   //e.target.parentElement.parentElement is the selectedUserFrame
 		userToChange.isAdmin = true;
 		console.log(userToChange);
 		reloadSelectedUserFrameWithCurrentUser();
 	}
+}
+
+function censorText(text) {
+	let result = '';
+	for (let i = 0; i < text.length; i++) {
+		result += '*';
+	}
+	return result;
 }
 
 
@@ -213,7 +222,13 @@ function createPasswordTextForm(text, parentDiv) {
 	selectedUserPasswordFrame.className = 'selectedUserPasswordFrame';
 	const selectedUserPasswordSubtitle = document.createElement('strong');
 	selectedUserPasswordSubtitle.innerText = 'Password: ';
-	const selectedUserPasswordText = document.createTextNode(text);
+	let selectedUserPasswordText = document.createTextNode('a');
+	if (parentDiv.user.isAdmin) {
+		selectedUserPasswordText = document.createTextNode(censorText(text));
+	} else {
+		selectedUserPasswordText = document.createTextNode(text);
+	}
+	console.log(selectedUserPasswordText);
 	selectedUserPasswordText.className = 'selectedUserPasswordText';
 	selectedUserPasswordFrame.append(selectedUserPasswordSubtitle);
 	selectedUserPasswordFrame.appendChild(selectedUserPasswordText);
@@ -249,6 +264,7 @@ function editPasswordClicked(e) {
 	const inputTextField = document.createElement('input');
 	inputTextField.setAttribute('type', 'text');
 	inputTextField.setAttribute('placeholder', originalText);
+	inputTextField.style.float = 'left';
 	inputTextField.className = 'inputTextField';
 
 	const saveChangesButton = document.createElement('div');
@@ -256,8 +272,14 @@ function editPasswordClicked(e) {
 	saveChangesButton.innerText = 'Save Changes';
 	saveChangesButton.addEventListener('click', saveChangesClicked);
 
+	const cancelChangesButton = document.createElement('div');
+	cancelChangesButton.className = 'cancelChangesButton';
+	cancelChangesButton.innerText = 'X';
+	cancelChangesButton.addEventListener('click', cancelChangesClicked);
+
 	frame.appendChild(inputTextField);
 	frame.appendChild(saveChangesButton);
+	frame.appendChild(cancelChangesButton);
 	
 }
 
@@ -275,11 +297,26 @@ function saveChangesClicked(e) {
 	while (frame.firstChild) {
 		frame.removeChild(frame.firstChild);
 	}
-	console.log(newText);
 
 	const parentDiv = frame.parentElement;
+	const affectedUser = parentDiv.user;
+	affectedUser.password = newText;
 	createPasswordTextForm(newText, parentDiv);
 
+}
+
+function cancelChangesClicked(e) {
+	const frame = e.target.parentElement;
+	let currentNode = frame.firstChild;
+	while (currentNode != null && (!currentNode.classList.contains('inputTextField'))) {
+		currentNode = currentNode.nextSibling;
+	}
+	while (frame.firstChild) {
+		frame.removeChild(frame.firstChild);
+	}
+	const originalText = currentNode.placeholder;
+	const parentDiv = frame.parentElement;
+	createPasswordTextForm(originalText, parentDiv);
 }
 
 
@@ -334,7 +371,7 @@ function reloadSelectedUserFrameWithCurrentUser() {
 		if (childNode.user == currentUser) {
 			childNode.click();
 		}
-		childNode= childNode.nextSibling;
+		childNode = childNode.nextSibling;
 	}
 
 }
